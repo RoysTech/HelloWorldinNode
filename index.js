ceb10096bc871d57543b39e30c38d85de780256c
RoysTech <104363352+RoysTech@users.noreply.github.com>
2023-04-26 13:54:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const http = require('http');
 
-/const hostname = 'localhost';
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -10,5 +9,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running on port ${port}`);
 });
