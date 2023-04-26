2aa2e5580188f2d84cad62dc6cf590d655266059
RoysTech <104363352+RoysTech@users.noreply.github.com>
2023-04-26 14:22:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-const hostname = 'https://hostinghelloworld.azurewebsites.net/';
+const hostname = 'localhost';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
@@ -9,6 +9,6 @@ const server = http.createServer((req, res) => {
   res.end('Hello, World!\n');
 });
 
-server.listen(port, () => {
-  console.log(`Server running `);
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
